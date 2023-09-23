4b8c3d7b83fe0f1eb40c30ad94b76ca45e5c0844
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2023-09-23 13:40:49 +0900

    Add process to parse HTML at input url

--- app/src/main/java/com/example/parse_html_using_jsoup/MainActivity.kt
+++ app/src/main/java/com/example/parse_html_using_jsoup/MainActivity.kt
@@ -3,53 +3,87 @@ package com.example.parse_html_using_jsoup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.parse_html_using_jsoup.ui.theme.ParsehtmlusingjsoupTheme
 import org.jsoup.Jsoup
 
 class MainActivity : ComponentActivity() {
+    private val _url = MutableLiveData<String>()
+    private val url: LiveData<String> = _url
+    private val _html = MutableLiveData<String>()
+    private val html: LiveData<String> = _html
+
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             ParsehtmlusingjsoupTheme {
-                var html2 by remember { mutableStateOf("") }
-
-                Thread {
-                    val url = "https://news.yahoo.co.jp/ranking/access/news"
-                    val doc = Jsoup.connect(url).get()
-
-                    html2 = doc.body().toString()
-                }.start()
-
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LazyColumn(content = {
-                        item {
-                            Text(
-                                text = getHtmlBody(html2)
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        var inputUrl by remember{ mutableStateOf("") }
+                        TextField(
+                            value = inputUrl,
+                            onValueChange = { text ->
+                                inputUrl = text
+                            },
+                            keyboardOptions = KeyboardOptions.Default.copy(
+                                imeAction = ImeAction.Done // 完了ボタンを表示
+                            ),
+                            keyboardActions = KeyboardActions(
+                                onDone = {
+                                    parseHtmFromUrl(inputUrl)
+                                }
                             )
-                        }
-                    })
+                        )
+                        Spacer(modifier = Modifier.padding(vertical = 20.dp))
+                        LazyColumn(content = {
+                            item {
+                                val observeHtml = html.observeAsState()
+                                Text(
+                                    text = observeHtml.value ?: "value in null"
+                                )
+                            }
+                        })
+                    }
                 }
             }
         }
     }
+    private fun parseHtmFromUrl(inputUrl: String) {
+        _url.value = inputUrl
 
-    fun getHtmlBody(value: String?): String {
-        return value ?: "value is null"
+        Thread {
+            val doc = Jsoup.connect(url.value).get()
+            _html.postValue(doc.body().toString())
+        }.start()
     }
 }

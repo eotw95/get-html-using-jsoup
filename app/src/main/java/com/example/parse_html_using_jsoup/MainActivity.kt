472026d6da770736e7b31604b381c9c899f6ecf4
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2023-09-23 11:53:42 +0900

    Fix code

--- app/src/main/java/com/example/parse_html_using_jsoup/MainActivity.kt
+++ app/src/main/java/com/example/parse_html_using_jsoup/MainActivity.kt
@@ -3,28 +3,17 @@ package com.example.parse_html_using_jsoup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.example.parse_html_using_jsoup.ui.theme.ParsehtmlusingjsoupTheme
 import org.jsoup.Jsoup
 
@@ -32,9 +21,6 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val _html = MutableLiveData<String>()
-        val html: LiveData<String> = _html
 
         setContent {
             ParsehtmlusingjsoupTheme {
